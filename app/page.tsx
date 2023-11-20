4064ec8e1e66865c751fd16792f93b797cc5e1e2
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-11-20 14:11:38 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -80,8 +80,8 @@ export default function Home() {
         <div
           className="p-4"
         >
-          <h1 className="text-gray-100 text-3xl font-bold">ReBAC Explorer</h1>
-          <h2 className="text-gray-400 text-xs">Edit, preview, and save your Descope ReBAC Schema. Try different example schemas for <a href="/exampleSchemas/courses.yaml" target="_blank" className="underline">courses</a>, <a href="/exampleSchemas/inventory.yaml" target="_blank" className="underline">inventory</a> 
+          <h1 className="text-gray-100 text-3xl font-bold">Descope ReBAC Explorer</h1>
+          <h2 className="text-gray-400 text-xs">Edit, preview, and save your ReBAC Schema. Try different example schemas for <a href="/exampleSchemas/courses.yaml" target="_blank" className="underline">courses</a>, <a href="/exampleSchemas/inventory.yaml" target="_blank" className="underline">inventory</a> 
           , <a href="/exampleSchemas/projects.yaml" target="_blank" className="underline">projects</a>
           , and <a href="/exampleSchemas/eCommerce.yaml" target="_blank" className="underline">E-commerce</a>.
           Check out the <a href="https://docs.descope.com/manage/authorization/rebac/" target="_blank" className="underline">ReBAC docs</a> for more information.
